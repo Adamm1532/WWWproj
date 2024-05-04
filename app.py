cb55a5ab2b3c408d3cc9ef1432593123cf038059
Adamm1532 <madon@student.agh.edu.pl>
2024-05-04 18:54:58 +0200

    Welp, something is brewing!

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def base():
+    return redirect(url_for('mario'))
 
 @app.route('/hello/<name>')
 def hello_name(name):
    return 'Hello %s!' % name
 
+@app.route('/mario')
+def mario():
+    return render_template('mario.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
