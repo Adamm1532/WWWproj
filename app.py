7be9f0227d39eae3ff8b4d0e179a48580cf671ab
Adamm1532 <madon@student.agh.edu.pl>
2024-06-11 15:15:38 +0200

    Login i think

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
-from flask import Flask, render_template, jsonify
-import datetime
+from flask import Flask, render_template
 from models import db, Mario_Speedruns, Celeste_Speedruns, Hollow_Knight_Speedruns, User, Role
-from flask_security import SQLAlchemyUserDatastore, Security
+from flask_security import SQLAlchemyUserDatastore, Security, hash_password, roles_required, verify_password
+from wtforms import StringField, PasswordField, validators
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.sqlite"
 app.config['SECRET_KEY'] = 'hello mario'
+app.config['SECURITY_PASSWORD_SALT'] = '882777363'
 db.init_app(app)
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-app.security = Security(app, user_datastore)
+app.security = Security(app, user_datastore, register_blueprint=False)
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
-    num_rows_deleted = db.session.query(Mario_Speedruns).delete()
-    num_rows_deleted2 = db.session.query(Celeste_Speedruns).delete()
-    db.session.commit()
-    obj = Mario_Speedruns(user='Suigi', time=datetime.timedelta(minutes=14, seconds=35, milliseconds=500),
-                          date=datetime.datetime(2023, 2, 22), link='https://youtu.be/ngMFEeDoX54', verified=True)
-    db.session.add(obj)
-    obj2 = Mario_Speedruns(user='Slipperynip', time=datetime.timedelta(minutes=14, seconds=41, milliseconds=210),
-                          date=datetime.datetime(2023, 11, 21), link='https://player.twitch.tv/?video=v1984963404&amp;parent=www.speedrun.com', verified=True)
-    db.session.add(obj2)
-    obj3 = Celeste_Speedruns(user='secureaccount', time=datetime.timedelta(minutes=24, seconds=58, milliseconds=23),
-                          date=datetime.datetime(2024, 3, 12), link='https://youtu.be/jNE6lSSbYNM', verified=True)
-    db.session.add(obj3)
-    obj4 = Celeste_Speedruns(user='Kezaron', time=datetime.timedelta(minutes=25, seconds=53, milliseconds=528),
-                             date=datetime.datetime(2024, 4, 6), link='https://youtu.be/NDUJPdAu6vk', verified=True)
-    db.session.add(obj4)
-    db.session.commit()
+    if not user_datastore.find_user(email="test@agh.edu.pl"):
+        moderator = user_datastore.create_role(name='moderator', description='Moderator')
+        db.session.add(moderator)
+        db.session.commit()
+        user = user_datastore.create_user(
+            email="test@agh.edu.pl",
+            username="test",
+            password=hash_password("test567"),
+        )
+        user_datastore.add_role_to_user(user, 'moderator')
+        db.session.commit()
 
 
 @app.route('/', methods=['GET'])
@@ -35,23 +33,39 @@ def base():
     mario_speedruns = Mario_Speedruns.query.order_by(Mario_Speedruns.time)
     celeste_speedruns = Celeste_Speedruns.query.order_by(Celeste_Speedruns.time)
     hollow_knight_speedruns = Hollow_Knight_Speedruns.query.order_by(Hollow_Knight_Speedruns.time)
-    return render_template('base.html', mario_speedruns=mario_speedruns, celeste_speedruns=celeste_speedruns,
+    return render_template('speedruns.html', mario_speedruns=mario_speedruns, celeste_speedruns=celeste_speedruns,
                            hollow_knight_speedruns=hollow_knight_speedruns)
 
-@app.route('/table/mario')
-def table1():
-    data = Mario_Speedruns.query.all()
-    return jsonify(data)
+@app.route('/waistlist', methods=['GET'])
+@roles_required('moderator')
+def waistlist():
+    mario_speedruns = Mario_Speedruns.query.order_by(Mario_Speedruns.time)
+    celeste_speedruns = Celeste_Speedruns.query.order_by(Celeste_Speedruns.time)
+    hollow_knight_speedruns = Hollow_Knight_Speedruns.query.order_by(Hollow_Knight_Speedruns.time)
+    return render_template('speedruns.html', mario_speedruns=mario_speedruns, celeste_speedruns=celeste_speedruns,
+                           hollow_knight_speedruns=hollow_knight_speedruns)
 
-@app.route('/table/celeste')
-def table2():
-    data = Celeste_Speedruns.query.all()
-    return jsonify(data)
+class LoginForm(FlaskForm):
+    email = StringField('Email', [validators.DataRequired("Please enter your email"), validators.Email()])
+    password = PasswordField('Password', [validators.DataRequired("Please enter password")])
 
-@app.route('/table/hollowknight')
-def table3():
-    data = Hollow_Knight_Speedruns.query.all()
-    return jsonify(data)
+# @app.route('/login', methods=['GET'])
+# def login():
+
+#     return render_template('login.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        print(user)
+        if user and verify_password(form.password.data, user.password):
+            print('Success')
+        else:
+            print('Failure')
+    return render_template('login.html', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -8,56 +8,62 @@ db = SQLAlchemy()
 
 class RolesUsers(db.Model):
     __tablename__ = 'roles_users'
-    id      = mapped_column(sa.Integer(), primary_key=True)
+    id = mapped_column(sa.Integer(), primary_key=True)
     user_id = mapped_column(sa.Integer(), sa.ForeignKey('user.id'))
     role_id = mapped_column(sa.Integer(), sa.ForeignKey('role.id'))
 
 class Role(db.Model, RoleMixin):
     __tablename__ = 'role'
-    id          = mapped_column(sa.Integer(), primary_key=True)
-    name        = mapped_column(sa.String(80), unique=True)
+    id = mapped_column(sa.Integer(), primary_key=True)
+    name = mapped_column(sa.String(80), unique=True)
     description = mapped_column(sa.String(255))
-    users       = relationship('User', secondary='roles_users', back_populates="roles", lazy=True)
+    users = relationship('User', secondary='roles_users', back_populates="roles", lazy=True)
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
-    id                = mapped_column(sa.Integer(), primary_key=True)
-    email             = mapped_column(sa.String(255), unique=True)
-    username          = mapped_column(sa.String(255), unique=True, nullable=True)
-    password          = mapped_column(sa.String(255), nullable=False)
-    last_login_at     = mapped_column(sa.DateTime())
-    current_login_at  = mapped_column(sa.DateTime())
-    last_login_ip     = mapped_column(sa.String(100))
+    id = mapped_column(sa.Integer(), primary_key=True)
+    email = mapped_column(sa.String(255), unique=True)
+    username = mapped_column(sa.String(255), unique=True, nullable=True)
+    password = mapped_column(sa.String(255), nullable=False)
+    last_login_at = mapped_column(sa.DateTime())
+    current_login_at = mapped_column(sa.DateTime())
+    last_login_ip = mapped_column(sa.String(100))
     current_login_ip = mapped_column(sa.String(100))
     login_count = mapped_column(sa.Integer())
     active = mapped_column(sa.Boolean())
     fs_uniquifier = mapped_column(sa.String(255), unique=True, nullable=False)
     confirmed_at = mapped_column(sa.DateTime())
     roles = relationship('Role', secondary='roles_users', back_populates="users", lazy=True)
+    mario_speedruns = relationship('Mario_Speedruns', back_populates='user', lazy=True)
+    celeste_speedruns = relationship('Celeste_Speedruns', back_populates='user', lazy=True)
+    hollow_knight_speedruns = relationship('Hollow_Knight_Speedruns', back_populates='user', lazy=True)
 
 class Mario_Speedruns(db.Model):
     __tablename__ = 'mario_speedruns'
     id = mapped_column(sa.Integer, primary_key=True)
-    user = mapped_column(sa.String(255), nullable=False)
+    user_id = mapped_column(sa.Integer(), sa.ForeignKey('user.id'), nullable=False)
     time = mapped_column(sa.Interval(), nullable=False)
     date = mapped_column(sa.DateTime(), nullable=False, default=datetime.datetime.now(datetime.timezone.utc))
-    link = mapped_column(sa.String(255), nullable=False)
+    link_id = mapped_column(sa.String(255), nullable=False)
     verified = mapped_column(sa.Boolean(), nullable=False, default=False)
+    user = relationship("User", back_populates='mario_speedruns', lazy=False)
 
 class Celeste_Speedruns(db.Model):
     __tablename__ = 'celeste_speedruns'
     id = mapped_column(sa.Integer, primary_key=True)
-    user = mapped_column(sa.String(255), nullable=False)
+    user_id = mapped_column(sa.Integer(), sa.ForeignKey('user.id'), nullable=False)
     time = mapped_column(sa.Interval(), nullable=False)
     date = mapped_column(sa.DateTime(), nullable=False, default=datetime.datetime.now(datetime.timezone.utc))
-    link = mapped_column(sa.String(255), nullable=False)
+    link_id = mapped_column(sa.String(255), nullable=False)
     verified = mapped_column(sa.Boolean(), nullable=False, default=False)
+    user = relationship("User", back_populates='celeste_speedruns', lazy=False)
 
 class Hollow_Knight_Speedruns(db.Model):
     __tablename__ = 'hollow_knight_speedruns'
     id = mapped_column(sa.Integer, primary_key=True)
-    user = mapped_column(sa.String(255), nullable=False)
+    user_id = mapped_column(sa.Integer(), sa.ForeignKey('user.id'), nullable=False)
     time = mapped_column(sa.Interval(), nullable=False)
     date = mapped_column(sa.DateTime(), nullable=False, default=datetime.datetime.now(datetime.timezone.utc))
-    link = mapped_column(sa.String(255), nullable=False)
+    link_id = mapped_column(sa.String(255), nullable=False)
     verified = mapped_column(sa.Boolean(), nullable=False, default=False)
+    user = relationship("User", back_populates='hollow_knight_speedruns', lazy=False)
